0d337c8d76ea36d62af8b70e8052a028cea1a3ee
Francesca Giordano <francesca020406@hotmail.com>
2017-11-13 21:15:36 -0500

    can't view details

--- src/app/phone-details/phone-details.component.ts
+++ src/app/phone-details/phone-details.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PhoneService } from './../phone.service';
 
 @Component({
   selector: 'app-phone-details',
   templateUrl: './phone-details.component.html',
   styleUrls: ['./phone-details.component.css'],
+  providers: [PhoneService],
 })
 export class PhoneDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private phoneService: PhoneService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getPhoneDetails(params['id']);
+    });
+  }
+
+  getPhoneDetails(id) {
+    this.phoneService.get(id)
+      .subscribe((phone) => {
+        this.phone = phone;
+      });
   }
 
 }
