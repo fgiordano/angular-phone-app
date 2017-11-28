ff4bb5f996b0e523faa0478f2ace439c2b7ad722
Francesca Giordano <francesca020406@hotmail.com>
2017-11-27 20:07:27 -0500

    adds route and form to add phones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PhoneListComponent } from './phone-list/phone-list.component';
 import { PhoneDetailsComponent } from './phone-details/phone-details.component';
-import {PhoneService} from './phone.service'
+import {PhoneService} from './phone.service';
+import { FileSelectDirective } from "ng2-file-upload";
+import { AddPhoneComponent } from './add-phone/add-phone.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PhoneListComponent,
-    PhoneDetailsComponent
+    PhoneDetailsComponent,
+    FileSelectDirective,
+    AddPhoneComponent
   ],
   imports: [
     BrowserModule,
